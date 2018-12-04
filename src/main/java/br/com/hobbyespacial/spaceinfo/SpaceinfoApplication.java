03445aaf5b6dd4aeadfc386aaef153e713715d63
wellington soares nunes <well_002@yahoo.com.br>
2018-12-04 21:00:10 -0200

    Configurações maven

---
+++ src/main/java/br/com/hobbyespacial/spaceinfo/SpaceinfoApplication.java
@@ -0,0 +1,12 @@
+package br.com.hobbyespacial.spaceinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceinfoApplication.class, args);
+	}
+}
