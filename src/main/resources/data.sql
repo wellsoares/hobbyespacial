7343d9cf9d11ae7fdf85cd093c5abbdbe0404adf
wellington soares nunes <well_002@yahoo.com.br>
2018-12-05 13:58:47 -0200

    Alteração de Script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+DELETE FROM planeta;
+DELETE FROM lua;
+
 insert into planeta VALUES ((SELECT PLANETA_SEQ.nextval FROM DUAL),'Mercurio',22.50);
 insert into planeta VALUES ((SELECT PLANETA_SEQ.nextval FROM DUAL),'Venus',22.50);
 insert into planeta VALUES ((SELECT PLANETA_SEQ.nextval FROM DUAL),'Terra',22.50);
