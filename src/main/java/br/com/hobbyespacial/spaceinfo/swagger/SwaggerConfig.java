0bda297198a3fbeb65e0924b102c1be5ee1496a0
wellington soares nunes <well_002@yahoo.com.br>
2018-12-05 23:43:35 -0200

    Inclusão Swagger

---
+++ src/main/java/br/com/hobbyespacial/spaceinfo/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package br.com.hobbyespacial.spaceinfo.swagger;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.hobbyespacial.spaceinfo.recursos"))
+				.paths(PathSelectors.ant("/planeta.*")).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("API REST", "API para Consulta de planetas", "API Planetas", "Termos de Serviço",
+				new Contact("Wellington S. Nunes", "", "well.soares.nunes@gmail.com"), "Licença de API",
+				"API license URL", Collections.emptyList());
+	}
+}
