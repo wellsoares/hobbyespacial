f78ff7d0a759b7a938e2c438b351f8fd374fbe5d
wellington soares nunes <well_002@yahoo.com.br>
2018-12-05 23:44:53 -0200

    Alteração de Script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,8 @@ insert into planeta VALUES ((SELECT PLANETA_SEQ.nextval FROM DUAL),'Mercurio',22
 insert into planeta VALUES ((SELECT PLANETA_SEQ.nextval FROM DUAL),'Venus',22.50);
 insert into planeta VALUES ((SELECT PLANETA_SEQ.nextval FROM DUAL),'Terra',22.50);
 insert into planeta VALUES ((SELECT PLANETA_SEQ.nextval FROM DUAL),'Marte',22.50);
+
+
+insert into planeta VALUES (999999,'TESTE',22.50);
+
+insert into lua VALUES (999999,SELECT MAX(ID) FROM planeta,'TESTE LUA',11.500);
